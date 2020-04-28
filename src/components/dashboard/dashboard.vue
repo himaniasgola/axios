b0f17f2e5b6c7b0c2cf499fba6d44e46a769c967
himaniasgola <62131249+himaniasgola@users.noreply.github.com>
2020-04-28 10:40:00 +0530

    Add files via upload

---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  created() {
+    axios.get('https://axiosproject-38470.firebaseio.com/users.json')
+    .then(res => console.log(res))
+    .catch(res => console.log(error))
+  }
+}
+</script>
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
